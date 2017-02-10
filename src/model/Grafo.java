a2097127ff889b98269c64d17a2ad8e0bbc194b4
Filipy <filipy.damaceno@gmail.com>
2017-02-10 11:14:57 -0200

    Função CriarMatrizIncidência.

--- src/model/Grafo.java
+++ src/model/Grafo.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package model;
 
 import java.util.ArrayList;
@@ -14,12 +13,11 @@ import model.Aresta;
  * @author Aluno
  */
 public class Grafo {
-    
+
     private String id;
     private TipoGrafo ordenacao;
     private ArrayList<Vertice> listaVertice = new ArrayList<Vertice>();
     private ArrayList<Aresta> listaAresta = new ArrayList<Aresta>();
-    
 
     public String getNome() {
         return id;
@@ -28,11 +26,11 @@ public class Grafo {
     public Grafo(String id, TipoGrafo ordenacao) {
         this.id = id;
         this.ordenacao = ordenacao;
-        
+
     }
 
     public Grafo() {
-        
+
     }
 
     public ArrayList<Vertice> getListaVertice() {
@@ -40,7 +38,7 @@ public class Grafo {
     }
 
     public void setListaVertice(Vertice v) {
-       listaVertice.add(v);
+        listaVertice.add(v);
     }
 
     public TipoGrafo getOrdenacao() {
@@ -59,55 +57,100 @@ public class Grafo {
         return listaAresta;
     }
 
-    public void setListaAresta(Aresta a ) {
+    public void setListaAresta(Aresta a) {
         this.listaAresta.add(a);
     }
 
-    public int[][] criarMatrizAdjacencia(Grafo a){
-    int[][] v = new int[a.getListaVertice().size()][a.getListaVertice().size()];
-    for(int i=0;i<a.getListaVertice().size();i++){
-        for(int j = 0; i <a.getListaVertice().size();i++){
-            v[i][j] = 0;
+    public int[][] criarMatrizAdjacencia(Grafo a) {
+        int[][] v = new int[a.getListaVertice().size()][a.getListaVertice().size()];
+        for (int i = 0; i < a.getListaVertice().size(); i++) {
+            for (int j = 0; i < a.getListaVertice().size(); i++) {
+                v[i][j] = 0;
+            }
+
         }
-    
-    }  
-    if(a.getOrdenacao() == TipoGrafo.unidirected){
-    for(int i=0;i<a.getListaVertice().size();i++){
-        String vertice1 = a.getListaVertice().get(i).getNome();
-        for(int j=0;j<a.getListaAresta().size();j++){
-            if(vertice1.equals(a.getListaAresta().get(j).getSource())){
-                for(int k = 0; k<a.getListaVertice().size();k++){
-                    String vertice2 = a.getListaVertice().get(k).getNome();
-                    if(vertice1.equals(a.getListaAresta().get(j).getSource()) && vertice2.equals(a.getListaAresta().get(j).getTarget())){
-                        v[i][k] = 1;
-                        v[k][i] = 1;
-                        break;
-                }
+        if (a.getOrdenacao() == TipoGrafo.unidirected) {
+            for (int i = 0; i < a.getListaVertice().size(); i++) {
+                String vertice1 = a.getListaVertice().get(i).getNome();
+                for (int j = 0; j < a.getListaAresta().size(); j++) {
+                    if (vertice1.equals(a.getListaAresta().get(j).getSource())) {
+                        for (int k = 0; k < a.getListaVertice().size(); k++) {
+                            String vertice2 = a.getListaVertice().get(k).getNome();
+                            if (vertice1.equals(a.getListaAresta().get(j).getSource()) && vertice2.equals(a.getListaAresta().get(j).getTarget())) {
+                                v[i][k] = 1;
+                                v[k][i] = 1;
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-    }
-    
-    if(a.getOrdenacao() == TipoGrafo.directed){
-    for(int i=0;i<a.getListaVertice().size();i++){
-        String vertice1 = a.getListaVertice().get(i).getNome();
-        for(int j=0;j<a.getListaAresta().size();j++){
-            if(vertice1.equals(a.getListaAresta().get(j).getSource())){
-                for(int k = 0; k<a.getListaVertice().size();k++){
-                    String vertice2 = a.getListaVertice().get(k).getNome();
-                    if(vertice1.equals(a.getListaAresta().get(j).getSource()) && vertice2.equals(a.getListaAresta().get(j).getTarget())){
-                        v[i][k] = 1;
-                        break;
-                }
+
+        if (a.getOrdenacao() == TipoGrafo.directed) {
+            for (int i = 0; i < a.getListaVertice().size(); i++) {
+                String vertice1 = a.getListaVertice().get(i).getNome();
+                for (int j = 0; j < a.getListaAresta().size(); j++) {
+                    if (vertice1.equals(a.getListaAresta().get(j).getSource())) {
+                        for (int k = 0; k < a.getListaVertice().size(); k++) {
+                            String vertice2 = a.getListaVertice().get(k).getNome();
+                            if (vertice1.equals(a.getListaAresta().get(j).getSource()) && vertice2.equals(a.getListaAresta().get(j).getTarget())) {
+                                v[i][k] = 1;
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
+
+        return v;
     }
+
+    public int[][] criarMatrizIncidencia(Grafo a) {
+        int[][] v = new int[a.getListaVertice().size()][a.getListaAresta().size()];
+        for (int i = 0; i < a.getListaVertice().size(); i++) {
+            for (int j = 0; i < a.getListaVertice().size(); i++) {
+                v[i][j] = 0;
+            }
+
+        }
+        if (a.getOrdenacao() == TipoGrafo.unidirected) {
+            for (int i = 0; i < a.getListaAresta().size(); i++) {
+                String vertice1 = a.getListaAresta().get(i).getSource();
+                String vertice2 = a.getListaAresta().get(i).getTarget();
+                for (int k = 0; k < a.getListaVertice().size(); k++) {
+                    if (vertice1.equals(a.getListaVertice().get(k).getNome())) {
+                        for (int j = 0; j < a.getListaVertice().size(); j++) {
+                            if (vertice2.equals(a.getListaVertice().get(j).getNome())) {
+                                v[k][i] = 1;
+                                v[j][i] = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (a.getOrdenacao() == TipoGrafo.directed) {
+            for (int i = 0; i < a.getListaAresta().size(); i++) {
+                String vertice1 = a.getListaAresta().get(i).getSource();
+                String vertice2 = a.getListaAresta().get(i).getTarget();
+                for (int k = 0; k < a.getListaVertice().size(); k++) {
+                    if (vertice1.equals(a.getListaVertice().get(k).getNome())) {
+                        for (int j = 0; j < a.getListaVertice().size(); j++) {
+                            if (vertice2.equals(a.getListaVertice().get(j).getNome())) {
+                                v[k][i] = 1;
+                                v[j][i] = -1;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return v;
     }
-        
-    return v;
-    }
-    
-    
+
 }
